f211b6ad8c5c3cd67483be6f1f50858dc1815629
Ojas Srivastava <srivastavaojas454@gmail.com>
2025-06-29 13:13:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 import express from "express";
-import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public")); // serve static files
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/submit", (req, res) => {
-  const bandName = req.body["street"] + req.body["pet"];
-  res.send(`<h1>Your band name is:</h1><h2>${bandName} ✌️</h2>`);
-});
-
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`🚀 Hack the Band is live on port ${port}`);
 });
